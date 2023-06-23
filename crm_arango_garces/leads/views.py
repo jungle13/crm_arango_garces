2701ded070c69c075343e13d0862d9caf7b64d11
CesarArangoC137 <ceau.0612@gmail.com>
2023-06-23 07:28:55 -0500

    segundo commit

--- crm_arango_garces/leads/views.py
+++ crm_arango_garces/leads/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
 def home_page(request):
     # return render(request, 'home.html')
     # return HttpResponse("<h1>Hello World</h1>")
-    return render(request, 'second_page.html')
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+    return render(request, 'second_page.html', context)
